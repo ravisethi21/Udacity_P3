83af9c083ac54e4e5e8b54c67bdb5090e87e1a5f
Ravi <ravisethi21@hotmail.com>
2021-01-15 13:50:14 +0530

    commit

--- create_cf.bat
+++ create_cf.bat
@@ -2,4 +2,6 @@ aws cloudformation deploy ^
 --template-file cloudfront.yml ^
 --stack-name production-distro ^
 --parameter-overrides PipelineID="cftestravi" ^
---tags project=udapeople
+--tags project=udapeople &
+
+01a33616-fa77-43cf-9ba2-789171be4084
